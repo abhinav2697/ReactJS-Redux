87031b0e678622b1123f5c8e10807f3c3b0443a4
abhinav2697 <abhinav2697@gmail.com>
2024-02-28 23:01:25 +0530

    new files and changes added

--- index.js
+++ index.js
@@ -1,70 +1,77 @@
-const redux = require("redux");
+const redux = require("redux")
+const thunk = require("redux-thunk").default
+
 const rootReducer = (state = { count: 0 }, action) => {
-    switch (action.type) {
+    switch(action.type) {
         case 'INCREMENT': {
             return {
                 count: state.count + 1
-                
             }
         }
-            
         case 'DECREMENT': {
             return {
                 count: state.count - 1
             }
         }
-            
         case 'INCREMENT_BY_VALUE': {
             return {
-                count:state.count+action.payload.value
+                count: state.count + action.payload.value
             }
         }
         case 'DECREMENT_BY_VALUE': {
             return {
-                count:state.count-action.payload.value
+                count: state.count - action.payload.value
             }
-            }
-            default:return state
+        }
+        default: return state
     }
-    console.log({
-        state,
-        action
-    })
 }
 
-const store=redux.createStore(rootReducer, {
-    count:0
-});
-
+const store = redux.createStore(rootReducer, {
+    count: 0
+},
+redux.applyMiddleware(thunk))
 
 const triggeredOnStoreUpdate = () => {
     const data = store.getState();
-    console.log(data);
+    console.log(data)
 }
-store.subscribe(triggeredOnStoreUpdate);
 
-store.dispatch({
-    type: "INCREMENT"
-})
+store.subscribe(triggeredOnStoreUpdate)
 
-store.dispatch({
-    type: "DECREMENT"
-})
-
-store.dispatch({
-    type: "INCREMENT_BY_VALUE",
-    payload: {
-        value:Math.floor(Math.random()*1000)
-        
+const incrementCounter = value => {
+    return (dispatch) => {
+        let type = "INCREMENT"
+        let payload = {}
+        if(value) {
+            type = "INCREMENT_BY_VALUE"
+            payload.value = value
+        }
+        dispatch({
+            type,
+            payload
+        })
     }
-})
+}
 
-
-store.dispatch({
-    type: "DECREMENT_BY_VALUE",
-    payload: {
-        value:Math.floor(Math.random()*1000)
-        
+const decrementCounter = value => {
+    return (dispatch) => {
+        let type = "DECREMENT"
+        let payload = {}
+        if(value) {
+            type = "DECREMENT_BY_VALUE"
+            payload.value = value
+        }
+        dispatch({
+            type,
+            payload
+        })
     }
-})
+}
 
+store.dispatch(incrementCounter())
+store.dispatch(decrementCounter())
+store.dispatch(incrementCounter(Math.floor(Math.random() * 1000)))
+store.dispatch(decrementCounter(Math.floor(Math.random() * 1000)))
+store.dispatch(decrementCounter(Math.floor(Math.random() * 1000)))
+store.dispatch(decrementCounter(Math.floor(Math.random() * 1000)))
